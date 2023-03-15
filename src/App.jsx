7c94335668f51949787c59c0c895f52c9b3a6e68
zaid shaikh <c-zaid.shaikh@timesgroup.com>
2023-03-15 16:48:51 +0530

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Coupon from "./Coupon";
 import Table from "./Table";
 import Header from "./Header";
-import Couponhidden from "./Couponhidden";
 import Filtertab from "./Filtertab";
 const App = () => {
   return (
@@ -14,7 +13,7 @@ const App = () => {
       {/* CARDS */}
       <div className="container mx-auto p-2">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
-          <Couponhidden hasButtons={false} />
+          <Coupon hasButtons={false} type="expired" hideDue={true} />
           <Coupon hasButtons={false} type="expired" />
           <Coupon hasButtons={false} type="expired" />
           <Coupon hasButtons={false} type="redeem" />
--- src/Coupon.jsx
+++ src/Coupon.jsx
@@ -6,30 +6,38 @@ const Coupon = (props) => {
             <div className="grid grid-cols-2 mb-2">
                 <div className="left-part">
                     <p className='text-base'><b>Bhoomika Arora</b></p>
-                    <p  className='text-xs'>ST NO-3 SHIVAJI PARK</p>
+                    <p className='text-xs'>ST NO-3 SHIVAJI PARK</p>
                     <p className='text-xs'>EKTP PH-4</p>
                     <p className='text-xs'>J-5/131</p>
                     <p className='text-xs'>Delhi-110032</p>
                 </div>
                 <div className="right-part text-right">
-                    <p className={`text-black ${props.type == "redeem" ? "text-green-400" : props.type == "due" ? "text-red-600" : props.type == "scanned" ? "text-violet-400" : props.type == "acknowledged" ? "text-blue-400" : props.type == "future" ? "text-black-400" : props.type == "expired" ? "text-orange-400" :  " text-base" }`}><b>EKT0034244X</b></p>
+                    <p className={`text-black ${props.type == "redeem" ? "text-green-400" : props.type == "due" ? "text-red-600" : props.type == "scanned" ? "text-violet-400" : props.type == "acknowledged" ? "text-blue-400" : props.type == "future" ? "text-black-400" : props.type == "expired" ? "text-orange-400" : " text-base"}`}><b>EKT0034244X</b></p>
                     <p className='text-xs'>OCT 2023</p>
                     <br />
                     <p className='text-xs'><b>THE TIMES OF INDIA</b></p>
-                    <p className={`text-black ${props.type == "redeem" ? "text-green-400" : props.type == "due" ? "text-red-600" : props.type == "scanned" ? "text-violet-400" : props.type == "acknowledged" ? "text-blue-400" : props.type == "future" ? "text-black-400" : props.type == "expired" ? "text-orange-400" :"text-xs"}`}><b>Due on 09/03/2022</b></p>
+                    {
+                        props.hideDue ?
+                            <></> :
+                            <p className={`text-black ${props.type == "redeem" ? "text-green-400" : props.type == "due" ? "text-red-600" : props.type == "scanned" ? "text-violet-400" : props.type == "acknowledged" ? "text-blue-400" : props.type == "future" ? "text-black-400" : props.type == "expired" ? "text-orange-400" : "text-xs"}`}><b>Due on 09/03/2022</b></p>
+                    }
                 </div>
             </div>
             <hr />
             <hr />
 
-            <div className="text-center text-sm mt-2 ">
-                <p>SHAHDARA / 0021692713 / 0021665321</p>
+            <div className={`text-center text-sm mt-2 ${props.hideDue ? 'lg:h-1/2 flex items-center' : ''}`}>
+                <p className={`${props.hideDue ? 'w-full' : ''}`}>SHAHDARA / 0021692713 / 0021665321</p>
             </div>
 
-            <div className="flex justify-between mt-2">
-                <button className='bg-green-300 text-white px-4 py-1 rounded'>Call Customer</button>
-                <button className='bg-blue-300 text-white px-4 py-1 rounded'>SMS Customer</button>
-            </div>
+            {
+                props.hideDue ?
+                    <></> :
+                    <div className="flex justify-between mt-2">
+                        <button className='bg-green-300 text-white px-4 py-1 rounded'>Call Customer</button>
+                        <button className='bg-blue-300 text-white px-4 py-1 rounded'>SMS Customer</button>
+                    </div>
+            }
         </div>
     )
 }
