367294cbaf18df96560dd1d71b6dbe3de13e1ac2
Rishank.Tiwari <jatinder.bhambri@timesgroup.com>
2023-03-15 14:50:23 +0530

    new changes

--- src/Coupon.jsx
+++ src/Coupon.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 const Coupon = (props) => {
     return (
-        <div className={`coupon border border-1-grey shadow rounded-xl p-2 ${props.type == "success" ? "bg-green-200" : props.type == "fail" ? "bg-red-200" : ""}`}>
+        <div className={`coupon border border-1-grey shadow rounded-xl p-2`}>
             <div className="grid grid-cols-2 mb-2">
                 <div className="left-part">
-                    <p><b>Bhoomika Arora</b></p>
-                    <p>ST NO-3 SHIVAJI PARK</p>
-                    <p>EKTP PH-4</p>
-                    <p>J-5/131</p>
-                    <p>Delhi-110032</p>
+                    <p className='text-base'><b>Bhoomika Arora</b></p>
+                    <p  className='text-xs'>ST NO-3 SHIVAJI PARK</p>
+                    <p className='text-xs'>EKTP PH-4</p>
+                    <p className='text-xs'>J-5/131</p>
+                    <p className='text-xs'>Delhi-110032</p>
                 </div>
                 <div className="right-part text-right">
-                    <p><b>EKT0034244X</b></p>
-                    <p>OCT 2023</p>
+                    <p className={`text-black ${props.type == "redeem" ? "text-green-400" : props.type == "due" ? "text-red-600" : props.type == "scanned" ? "text-violet-400" : props.type == "acknowledged" ? "text-blue-400" : props.type == "future" ? "text-black-400" : props.type == "expired" ? "text-red-400" :  " text-base" }`}><b>EKT0034244X</b></p>
+                    <p className='text-xs'>OCT 2023</p>
                     <br />
-                    <p><b>THE TIMES OF INDIA</b></p>
-                    <p className='text-red-400'><b>Due on 09/03/2022</b></p>
+                    <p className='text-xs'><b>THE TIMES OF INDIA</b></p>
+                    <p className='text-red-400 text-xs'><b>Due on 09/03/2022</b></p>
                 </div>
             </div>
             <hr />
             <hr />
 
-            <div className="text-center mt-2">
+            <div className="text-center text-sm mt-2 ">
                 <p>SHAHDARA / 0021692713 / 0021665321</p>
             </div>
 
--- src/Table.jsx
+++ src/Table.jsx
@@ -4,25 +4,26 @@ const Table = () => {
   return (
     <div className='container mx-auto p-2'>
       {/* Table */}
-      <table className='w-full border border-3-grey rounded'>
+      <table className='w-full border border-3-grey rounded-xl shadow-md '>
+        
         <tbody>
             <tr>
-                <td>Due</td>
-                <td className='border-1-grey'>28</td>
-                <td>Redeem</td>
-                <td>15</td>
+                <td className='text-red-600'>Due</td>
+                <td className='border-r text-red-600'>28</td>
+                <td className='text-green-400'>Redeem</td>
+                <td className='text-green-400'>15</td>
             </tr>
             <tr>
-                <td>Scanned</td>
-                <td>19</td>
-                <td>Acknowledged</td>
-                <td>0</td>
+                <td className='text-violet-400'>Scanned</td>
+                <td className='border-r text-violet-400'>19</td>
+                <td className='text-blue-400'>Acknowledged</td>
+                <td className='text-blue-400'>1</td>
             </tr>
             <tr>
-                <td>Future</td>
-                <td>14</td>
-                <td>Expired</td>
-                <td>0</td>
+                <td className='text-black'>Future</td>
+                <td className='border-r text-black'>14</td>
+                <td className='text-red-400'>Expired</td>
+                <td className='text-red-400'>0</td>
             </tr>
         </tbody>
       </table>
