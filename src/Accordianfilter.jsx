7b067ebc1f0d90b16c85e00d76fed18b5e32020a
Munna Kumar <mu20447312@wipro.com>
2023-03-20 15:46:48 +0530

    Wip

--- src/Accordianfilter.jsx
+++ src/Accordianfilter.jsx
@@ -1,36 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 
 const Accordianfilter = () => {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+  useEffect(() => {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }, []);
+
   return (
-    <div className="fontfamilyhome">
-      <Header />
-      <div className="border-2  shadow rounded-xl">
-        <div className="text-red-600 flex justify-center justify-items-center">
-          <p>Filter</p>
-        </div>
+    <div className="container mx-auto">
+      <div className="fontfamilyhome">
+        <Header />
+        <div className="border-2  shadow rounded-xl">
+          <div className="text-red-600 flex justify-center justify-items-center">
+            <p className="p-2">Filter</p>
+          </div>
 
-        <button className="accordion">Dealer</button>
-        <div className="panel">
-        <select className="option">
-          <option>Rishank</option>
-          <option>Mahesh</option>
-          <option>Jatin</option>
-        </select>
+          <button className="accordion">Dealer</button>
+          <div className="panel">
+            <select className="option">
+              <option>Rishank</option>
+              <option>Mahesh</option>
+              <option>Jatin</option>
+            </select>
+          </div>
+
+
+          <button className="accordion">Dealer</button>
+          <div className="panel">
+            <select className="option">
+              <option>Rishank</option>
+              <option>Mahesh</option>
+              <option>Jatin</option>
+            </select>
+          </div>
+
+          
+          <button className="accordion">Dealer</button>
+          <div className="panel">
+            <select className="option">
+              <option>Rishank</option>
+              <option>Mahesh</option>
+              <option>Jatin</option>
+            </select>
+          </div>
+
+          
+          <button className="accordion">Dealer</button>
+          <div className="panel">
+            <select className="option">
+              <option>Rishank</option>
+              <option>Mahesh</option>
+              <option>Jatin</option>
+            </select>
+          </div>
+
+
+
         </div>
       </div>
     </div>
